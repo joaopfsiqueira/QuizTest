19b2ce7fb1f7fff454fdfb35cb8c46f48225663e
João Pedro Siqueira <49644984+joaopfsiqueira@users.noreply.github.com>
2020-08-31 16:48:26 -0300

    Add files via upload

---
+++ QuizTeste/QuizTeste/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace QuizTeste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.p1Correta = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.p2Correta = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.lbl1Incorretas = new System.Windows.Forms.Label();
+            this.lbl2Incorretas = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(71, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1 - Qual é o resultado de 40 + 30?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(71, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(228, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2 - Qual é o resultado de 4 x 5?";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(154, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Conferir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(138, 400);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 22);
+            this.lblResultado.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.p1Correta);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(85, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(14, 16);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(37, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "50";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(14, 39);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(37, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "65";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // p1Correta
+            // 
+            this.p1Correta.AutoSize = true;
+            this.p1Correta.Location = new System.Drawing.Point(14, 62);
+            this.p1Correta.Name = "p1Correta";
+            this.p1Correta.Size = new System.Drawing.Size(37, 17);
+            this.p1Correta.TabIndex = 2;
+            this.p1Correta.TabStop = true;
+            this.p1Correta.Text = "70";
+            this.p1Correta.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.radioButton6);
+            this.panel2.Controls.Add(this.radioButton5);
+            this.panel2.Controls.Add(this.p2Correta);
+            this.panel2.Location = new System.Drawing.Point(85, 216);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 5;
+            // 
+            // p2Correta
+            // 
+            this.p2Correta.AutoSize = true;
+            this.p2Correta.Location = new System.Drawing.Point(14, 15);
+            this.p2Correta.Name = "p2Correta";
+            this.p2Correta.Size = new System.Drawing.Size(37, 17);
+            this.p2Correta.TabIndex = 0;
+            this.p2Correta.TabStop = true;
+            this.p2Correta.Text = "20";
+            this.p2Correta.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(14, 38);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(37, 17);
+            this.radioButton5.TabIndex = 1;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "30";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(14, 61);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(37, 17);
+            this.radioButton6.TabIndex = 2;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "25";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // lbl1Incorretas
+            // 
+            this.lbl1Incorretas.AutoSize = true;
+            this.lbl1Incorretas.Location = new System.Drawing.Point(136, 164);
+            this.lbl1Incorretas.Name = "lbl1Incorretas";
+            this.lbl1Incorretas.Size = new System.Drawing.Size(0, 13);
+            this.lbl1Incorretas.TabIndex = 6;
+            // 
+            // lbl2Incorretas
+            // 
+            this.lbl2Incorretas.AutoSize = true;
+            this.lbl2Incorretas.Location = new System.Drawing.Point(136, 329);
+            this.lbl2Incorretas.Name = "lbl2Incorretas";
+            this.lbl2Incorretas.Size = new System.Drawing.Size(0, 13);
+            this.lbl2Incorretas.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(91, 400);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 22);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Nota:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl2Incorretas);
+            this.Controls.Add(this.lbl1Incorretas);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton p1Correta;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton p2Correta;
+        private System.Windows.Forms.Label lbl1Incorretas;
+        private System.Windows.Forms.Label lbl2Incorretas;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ QuizTeste/QuizTeste/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizTeste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int resultado = 0;
+
+            if(p1Correta.Checked == true)  //checked é checado.
+            {
+                resultado = resultado + 5;
+                lbl1Incorretas.Text = "Resposta Correta";
+            }else{
+                lbl1Incorretas.Text = "Resposta incorreta";
+            }
+
+            if(p2Correta.Checked == true) 
+            {
+                resultado = resultado + 5;
+                lbl2Incorretas.Text = "Resposta Correta";
+            } else
+            {
+                lbl2Incorretas.Text = "Resposta Incorreta";
+            }
+
+            lblResultado.Text = resultado.ToString();
+
+        }
+    }
+}
